4da3429a96b5680b621627b3907bd81d38eabaae
dlee886 <dlee886@aucklanduni.ac.nz>
2024-04-03 21:16:38 +1300

    controllerV4
    Major animation code rework (complete structural change)
    -existing animations recoded using arrays
    -new animations using reworked functions
    
    Added brightness input setting - potentiometer

--- main/main.ino
+++ main/main.ino
@@ -9,23 +9,18 @@ Designed by: Damian
 
 #define NUM_LEDS 32  //pins & led strip
 #define LED_PIN A4
-#define LED_BRIGHTNESS 15
 #define PALETTE_BUTTON 3
 #define PATTERN_BUTTON 2
+#define BRIGHTNESS_POTENTIOMETER A3
 
 CRGB leds[NUM_LEDS];  //led strip array
 
-uint8_t currentPaletteCase = 0;  //buttons increment
+uint8_t rando[6];  //randomizer variables for color setting of some patterns
+uint8_t wavy;  //function variable for position setting of some patterns
+
+uint8_t currentPaletteCase = 0;  //input value holders
 uint8_t currentPatternCase = 0;
-
-uint8_t rand1;  //randomizer variables for color setting
-uint8_t rand2;
-uint8_t rand3;
-uint8_t wave1;  //function variables for position setting
-uint8_t wave2;
-uint8_t wave3;
-
-uint8_t index = 0;  //periodic incrementing value for some patterns
+uint8_t currentBrightness = 50;
 
 unsigned long lastDebounceTime = 0;  //timing gimmicks
 unsigned int debounceDelay = 250;
@@ -102,7 +97,7 @@ void setup() {  //code to run once
   delay(500);
 
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(LED_BRIGHTNESS);
+  FastLED.setBrightness(currentBrightness);
 
   pinMode(PALETTE_BUTTON, INPUT_PULLUP);
   pinMode(PATTERN_BUTTON, INPUT_PULLUP);
@@ -119,6 +114,11 @@ void setup() {  //code to run once
 //----------------------------------------------------------------
 
 void loop() {                    //code to run continuously
+
+  int brightnessPot = analogRead(BRIGHTNESS_POTENTIOMETER);
+  currentBrightness = map(brightnessPot, 0, 1023, 0, 240) + 15;
+  FastLED.setBrightness(currentBrightness);
+
   switch (currentPaletteCase) {  //color palette picker
     case 1:
       currentPalette = rainbow;
@@ -144,75 +144,74 @@ void loop() {                    //code to run continuously
     case 0:
       blinkPattern();
       break;
-    case 1:
-      flowPattern();
-      break;
+    case 1: //old pattern, now just a forced color switcher
+    if (currentPaletteCase == 0) {
+      currentPaletteCase = 1;
+    }
+    currentPatternCase++;
     case 2:
-      sawtoothPattern(60, 60, 60);
+      sawtoothPattern(60, 0, 0);
+      sawtoothPattern(60, 333, 1);
+      sawtoothPattern(60, 666, 2);
+
+      fadeToBlackBy(leds, NUM_LEDS, 8);
       break;
     case 3:
-      wavePattern(30, 30, 30);
+      wavePattern(30, 0, 0);
+      wavePattern(30, 85, 1);
+      wavePattern(30, 170, 2);
+      wavePattern(40, 0, 3);
+
+      fadeToBlackBy(leds, NUM_LEDS, 8);
       break;
     case 4:
-      sawtoothPattern(60, 81, 121);
-      wavePattern(30, 31, 41);
+      sawtoothPattern(120, 0, 0);
       break;
     case 5:
+      wavePattern(50, 0, 0);
+      break;
+    case 6:
+      sawtoothPattern(60, 0, 0);
+      sawtoothPattern(71, 0, 1);
+      sawtoothPattern(111, 0, 2);
+      sawtoothPattern(63, 333, 3);
+      sawtoothPattern(67, 666, 4);    
+
+      fadeToBlackBy(leds, NUM_LEDS, 18);
+      break;
+    case 7:
       sparklePattern();
+      fadeToBlackBy(leds, NUM_LEDS, 1);
       break;
   }
-
-  EVERY_N_MILLISECONDS(20) {
-    index += 16;
-  };
 
   FastLED.show();
 }
 
 //----------------------------------------------------------------
 void blinkPattern() { //pattern setting
-  fill_palette(leds, NUM_LEDS, index, 16, currentPalette, 255, LINEARBLEND);
-  delay(250);
-  FastLED.show();
   fill_solid(leds, NUM_LEDS, CRGB::Black);
   delay(250);
+  FastLED.show();
+  fill_palette(leds, NUM_LEDS, millis() / 500 * 16, 16, currentPalette, 255, LINEARBLEND);
+  delay(250);
 }
 
-void flowPattern() {
-  // if (currentPaletteCase == 0) {
-  //   currentPaletteCase = 1;
-  // }
-  fill_palette(leds, NUM_LEDS, index, 16, currentPalette, 255, LINEARBLEND);
+void sawtoothPattern(uint8_t bpm, int offset, uint8_t rand_index) {
+  wavy = map(beat8(bpm, offset), 0, 255, 0, NUM_LEDS - 1);
+
+  colorPicker(rand_index, wavy, 0);
+
+  leds[wavy] = ColorFromPalette(currentPalette, rando[rand_index]);
 }
 
-void sawtoothPattern(uint8_t bpm1, uint8_t bpm2, uint8_t bpm3) {
-  wave1 = map(beat8(bpm1, 0), 0, 255, 0, NUM_LEDS - 1);
-  wave2 = map(beat8(bpm2, 333), 0, 255, 0, NUM_LEDS - 1);
-  wave3 = map(beat8(bpm3, 666), 0, 255, 0, NUM_LEDS - 1);
+void wavePattern(uint8_t bpm, uint8_t phase, uint8_t rand_index) {
+  wavy = beatsin8(bpm, 0, NUM_LEDS - 1, 0, phase);
 
-  colorPicker(wave1, wave2, wave3, 0);
-  colorPicker(wave1, wave2, wave3, NUM_LEDS - 1);
+  colorPicker(rand_index, wavy, 0);
+  colorPicker(rand_index, wavy, NUM_LEDS - 1);
 
-  leds[wave1] = ColorFromPalette(currentPalette, rand1);
-  leds[wave2] = ColorFromPalette(currentPalette, rand2);
-  leds[wave3] = ColorFromPalette(currentPalette, rand3);
-
-  fadeToBlackBy(leds, NUM_LEDS, 8);
-}
-
-void wavePattern(uint8_t bpm1, uint8_t bpm2, uint8_t bpm3) {
-  wave1 = beatsin8(bpm1, 0, NUM_LEDS - 1, 0, 0);
-  wave2 = beatsin8(bpm2, 0, NUM_LEDS - 1, 0, 85);
-  wave3 = beatsin8(bpm3, 0, NUM_LEDS - 1, 0, 170);
-
-  colorPicker(wave1, wave2, wave3, 0);
-  colorPicker(wave1, wave2, wave3, NUM_LEDS - 1);
-
-  leds[wave1] = ColorFromPalette(currentPalette, rand1);
-  leds[wave2] = ColorFromPalette(currentPalette, rand2);
-  leds[wave3] = ColorFromPalette(currentPalette, rand3);
-
-  fadeToBlackBy(leds, NUM_LEDS, 8);
+  leds[wavy] = ColorFromPalette(currentPalette, rando[rand_index]);
 }
 
 void sparklePattern() {
@@ -220,7 +219,6 @@ void sparklePattern() {
     leds[random8(NUM_LEDS - 1)] = ColorFromPalette(currentPalette, random8());
     tick = millis();
   }
-  fadeToBlackBy(leds, NUM_LEDS, 1);
 }
 
 void cacPattern() {
@@ -228,25 +226,26 @@ void cacPattern() {
 
 
 //--------------------------------
-void colorPicker(uint8_t wav1, uint8_t wav2, uint8_t wav3, uint8_t i) { //pattern setting helpers
-  if (wav1 == i) {
-    rand1 = slowRandom8(rand1);
-  } else if (wav2 == i) {
-    rand2 = slowRandom8(rand2);
-  } else if (wav3 == i) {
-    rand3 = slowRandom8(rand3);
+void colorPicker(uint8_t rand_index, uint8_t wav, uint8_t i) { //pattern setting helpers
+  if (wav == i) {
+    rando[rand_index] = slowRandom8(rando[rand_index]);
   }
 }
 
-uint8_t slowRandom8(uint8_t rando) {
+uint8_t slowRandom8(uint8_t randy) {
   if (millis() - tick > 300) {
     tick = millis();
-    return random8();
+
+    uint8_t temp = random8();
+    if (abs(temp - randy) < 16) {
+      return random8();
+    } else {
+      return temp;
+    }
   } else {
-    return rando;
+    return randy;
   }
 }
-
 
 //input gimmicks
 void nextPalette() {
@@ -261,6 +260,6 @@ void nextPattern() {
   if ((millis() - lastDebounceTime) > debounceDelay) {
     lastDebounceTime = millis();
 
-    currentPatternCase = (currentPatternCase + 1) % 6;
+    currentPatternCase = (currentPatternCase + 1) % 8;
   }
 }
