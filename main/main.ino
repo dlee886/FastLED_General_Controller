3b71402ce048ab66d66c94371d33bbca86a2ed10
dlee886 <dlee886@aucklanduni.ac.nz>
2024-04-03 20:54:28 +1300

    controllerV2
    Added second button functionality - toggle selected animation (pattern).
    Issues with detecting button press (delay freezes controller and all pins)

--- main/main.ino
+++ main/main.ino
@@ -3,123 +3,152 @@
 
 #define NUM_LEDS 32
 #define LED_PIN A4
+#define LED_BRIGHTNESS 50
 
 CRGB leds[NUM_LEDS];
 
 uint8_t index = 0;
-uint8_t casePalette = 0;
+uint8_t currentPaletteCase = 0;
+uint8_t currentPatternCase = 1;
 
-Button nextPalette(2);
+Button nextPalette(3);
+Button nextPattern(2);
 
-DEFINE_GRADIENT_PALETTE (rainbow) {
-  0,   255,  0,  0,
-  31,  255, 191, 0,
-  63,  127, 255, 0,
-  95,    0, 255, 63,
-  127,   0, 255, 255,
-  159,  63,   0, 255,
-  191, 128,   0, 255,
-  223, 255,   0, 191,
-  255, 255,   0,   0
+DEFINE_GRADIENT_PALETTE(rainbow){
+  0, 255, 0, 0,
+  31, 255, 191, 0,
+  63, 127, 255, 0,
+  79, 0, 255, 0,
+  95, 0, 255, 63,
+  127, 0, 255, 255,
+  152, 0, 0, 255,
+  159, 63, 0, 255,
+  191, 128, 0, 255,
+  223, 255, 0, 191,
+  255, 255, 0, 0
 };
 
-DEFINE_GRADIENT_PALETTE (red) {
-  0,   255,   0, 0,
-  127, 255, 128,  0,
-  255, 255,   0, 0
+DEFINE_GRADIENT_PALETTE(red){
+  0, 255, 0, 0,
+  127, 255, 128, 0,
+  255, 255, 0, 0
 };
 
-DEFINE_GRADIENT_PALETTE (orange) {
-  0,    255, 191, 0,
-  127,  127, 255, 0,
-  255,  255, 191, 0
+DEFINE_GRADIENT_PALETTE(yellow){
+  0, 255, 255, 0,
+  127, 255, 63, 0,
+  255, 255, 255, 0
 };
 
-DEFINE_GRADIENT_PALETTE (yellow) {
-  0,   255,  255, 0,
-  127, 255, 127, 0,
-  255, 255,  255, 0
-};
-
-DEFINE_GRADIENT_PALETTE (green) {
-  0,   0, 255, 0,
-  127, 0, 255, 63,
+DEFINE_GRADIENT_PALETTE(green){
+  0, 0, 255, 0,
+  127, 255, 255, 0,
   255, 0, 255, 0
 };
 
-DEFINE_GRADIENT_PALETTE (blue) {
-  0,   0,  0, 255,
-  127, 0, 255,  255,
-  255, 0,  0, 255
+DEFINE_GRADIENT_PALETTE(blue){
+  0, 0, 0, 255,
+  127, 0, 255, 255,
+  255, 0, 0, 255
 };
 
-DEFINE_GRADIENT_PALETTE (purple) {
-  0,   127, 0, 255,
-  127, 255, 0,  63,
-  255, 127, 0, 255
+DEFINE_GRADIENT_PALETTE(purple){
+  0, 63, 0, 255,
+  127, 255, 0, 63,
+  255, 63, 0, 255
 };
 
-DEFINE_GRADIENT_PALETTE (pink) {
-  0,   255,  0, 255,
-  128, 255, 0,  128,
-  255, 255,  0, 255
-};
-CRGBPalette16 currentPalette = rainbow;
+CRGBPalette16 currentPalette(
+  CRGB::White,
+  CRGB::Black,
+  CRGB::White,
+  CRGB::Black,
+  CRGB::White,
+  CRGB::Black,
+  CRGB::White,
+  CRGB::Black,
+  CRGB::White,
+  CRGB::Black,
+  CRGB::White,
+  CRGB::Black,
+  CRGB::White,
+  CRGB::Black,
+  CRGB::White,
+  CRGB::Black);
 
-void setup() {
+  void setup() {
   // put your setup code here, to run once:
   delay(500);
 
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(50);
+  FastLED.setBrightness(LED_BRIGHTNESS);
 
   nextPalette.begin();
-  
+  nextPattern.begin();
+
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  EVERY_N_MILLISECONDS(8) {
-    if  (nextPalette.pressed()) {
-      if (casePalette == 7) {
-      casePalette = 0;
-    } else {
-      casePalette++;
-    }
-
-    switch (casePalette) {
-      case 0:
+  switch (currentPaletteCase) {
+    case 1:
       currentPalette = rainbow;
       break;
-      case 1:
+    case 2:
       currentPalette = red;
       break;
-      case 2:
-      currentPalette = orange;
-      break;
-      case 3:
+    case 3:
       currentPalette = yellow;
       break;
-      case 4:
+    case 4:
       currentPalette = green;
       break;
-      case 5:
+    case 5:
       currentPalette = blue;
       break;
-      case 6:
+    case 6:
       currentPalette = purple;
+      currentPaletteCase = 0;
+  }
+
+  switch (currentPatternCase) {
+    case 1:
+      blinkPattern();
       break;
-      case 7:
-      currentPalette = pink;
+    case 2:
+      wavePattern();
+      break;
+    case 3:
+      currentPatternCase = 1;
+      break;
+  }
+
+  EVERY_N_MILLISECONDS(8) {
+    if (nextPalette.pressed()) {
+      currentPaletteCase++;
+    }
+    if (nextPattern.pressed()) {
+      currentPatternCase++;
     }
   }
-}
-  
 
   EVERY_N_MILLISECONDS(20) {
-    fill_palette(leds, NUM_LEDS, index, 16, currentPalette, 255, LINEARBLEND);  
     index += 8;
-    FastLED.show();
   };
+
+  FastLED.show();
+}
+
+void blinkPattern() {
+  fill_palette(leds, NUM_LEDS, 0, 16, currentPalette, 255, LINEARBLEND);
+  delay(500);
+  FastLED.show();
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  delay(500);
+  
+}
+
+void wavePattern() {
+  fill_palette(leds, NUM_LEDS, index, 16, currentPalette, 255, LINEARBLEND);
 }
